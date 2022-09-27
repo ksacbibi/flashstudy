a20c66d7a0763051e952054da15ec7d92fd5c037
ksacbibi <34385626+ksacbibi@users.noreply.github.com>
2022-09-27 18:37:40 -0400

    Some card input logic

--- src/components/cardInput/CardInput.js
+++ src/components/cardInput/CardInput.js
@@ -1,12 +1,31 @@
 import './CardInput.css';
+import { useState } from 'react';
 
 export default function CardInput() {
+    const [wordInput, setWordInput] = useState(); // get the word inputted by user
+    const [definitionInput, setDefinitionInput] = useState(); // get definition inputted by user
+    const [card, setCard] = useState(); // card object that will contain word and definition
+    const [deck, setDeck] = useState([]); // deck will be an array of all cards
+
+    const handleButton = () => {
+        setCard({word: wordInput, definition: definitionInput});
+        setDeck(deck => [...deck, card]); // add new card to deck
+        // render flashcard... somehow..
+    }
+
+    const handleChange = e => {
+        if (e.target.id === "wordInput") {
+            setWordInput(e.target.value);
+        } else if (e.target.id === "definitionInput") {
+            setDefinitionInput(e.target.value);
+        }
+    }
     return (
         <div className="container">
             <div className="inputContainer">
                 <div className="inputs">
                     <label for="wordInput">Word: </label>
-                    <input type="text" id="wordInput"></input>
+                    <input type="text" id="wordInput" onChange={handleChange}></input>
                 </div>
                 <div className="inputs">
                     <label for="definitionInput">Definition: </label>
@@ -14,7 +33,7 @@ export default function CardInput() {
                 </div>
             </div>
             <div className="submitCardBtn">
-                <button id="submitCardBtn">Submit</button>
+                <button id="submitCardBtn" onclick={handleButton}>Submit</button>
             </div>
         </div>
     )
--- src/components/flashcard/Flashcard.js
+++ src/components/flashcard/Flashcard.js
@@ -1,9 +1,10 @@
 import './Flashcard.css';
+// import CardInput from '../cardInput/CardInput';
 
 export default function Flashcard() {
     return (
         <div className="card">
-            
+            {/* <CardInput/> */}
         </div>
     );
 }
